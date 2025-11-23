0d37cc893481fc2d4d6b6b0ca7ae3b55398de4b0
Yantsy <g17870797268@163.com>
2025-11-23 12:29:51 +0800

    创建项目

---
+++ src/home.cc
@@ -0,0 +1,8 @@
+#include "home.h"
+
+home::home(QWidget *parent) : QWidget(parent) {
+  this->setWindowTitle("Qt Video Player");
+  this->resize(800, 600);
+}
+
+home::~home() {}
---
+++ src/home.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QWidget>
+
+class home : public QWidget {
+  Q_OBJECT
+public:
+  home(QWidget *parent = nullptr);
+  ~home();
+
+private:
+private slots:
+};
